b4398f6e9000bb4bcc2872a59a470e883ed0a78c
darrell-k <darrell@darrell.org.uk>
2024-11-13 12:21:29 +0000

    Plural descriptions for user-defined tags

--- Slim/Web/Settings/Server/Behavior.pm
+++ Slim/Web/Settings/Server/Behavior.pm
@@ -110,16 +110,24 @@ sub handler {
 				if ( $tag ) {
 					$customTags->{$tag} = {
 						name => $paramRef->{$key . '_name'} || $tag,
+						namePlural => $paramRef->{$key . '_namePlural'} || $paramRef->{$key . '_name'} || $tag,
 						id => $userDefinedRoles->{$tag} ? $userDefinedRoles->{$tag}->{id} : $customRoleId++,
 						include => $userDefinedRoles->{$tag}  ? $userDefinedRoles->{$tag}->{include} : 1,
 						albumLink => $userDefinedRoles->{$tag}  ? $userDefinedRoles->{$tag}->{albumLink} : 1,
 					};
 
-					if ( !$userDefinedRoles->{$tag} || $userDefinedRoles->{$tag}->{name} ne $customTags->{$tag}->{name} ) {
-						Slim::Utils::Strings::storeExtraStrings([{
+					if ( !$userDefinedRoles->{$tag} || $userDefinedRoles->{$tag}->{name} ne $customTags->{$tag}->{name}
+									|| $userDefinedRoles->{$tag}->{namePlural} ne $customTags->{$tag}->{namePlural} ) {
+						Slim::Utils::Strings::storeExtraStrings(
+							[{
 							strings => { EN => $customTags->{$tag}->{name}},
 							token   => $tag,
-						}]);
+							},
+							{
+							strings => { EN => $customTags->{$tag}->{namePlural}},
+							token   => $tag . "_PLURAL",
+							}
+							]);
 						$changed = 1;
 					}
 				}
