702850448732f0cb02c80fdb3291c487eaf76a8e
darrell-k <darrell@darrell.org.uk>
2024-11-13 15:56:07 +0000

    Tidy up indents, default tag text to ucfirst

--- Slim/Web/Settings/Server/Behavior.pm
+++ Slim/Web/Settings/Server/Behavior.pm
@@ -109,8 +109,8 @@ sub handler {
 
 				if ( $tag ) {
 					$customTags->{$tag} = {
-						name => $paramRef->{$key . '_name'} || $tag,
-						namePlural => $paramRef->{$key . '_namePlural'} || $paramRef->{$key . '_name'} || $tag,
+						name => $paramRef->{$key . '_name'} || ucfirst(lc($tag)),
+						namePlural => $paramRef->{$key . '_namePlural'} || $paramRef->{$key . '_name'} || ucfirst(lc($tag)),
 						id => $userDefinedRoles->{$tag} ? $userDefinedRoles->{$tag}->{id} : $customRoleId++,
 						include => $userDefinedRoles->{$tag}  ? $userDefinedRoles->{$tag}->{include} : 1,
 						albumLink => $userDefinedRoles->{$tag}  ? $userDefinedRoles->{$tag}->{albumLink} : 1,
@@ -119,15 +119,11 @@ sub handler {
 					if ( !$userDefinedRoles->{$tag} || $userDefinedRoles->{$tag}->{name} ne $customTags->{$tag}->{name}
 									|| $userDefinedRoles->{$tag}->{namePlural} ne $customTags->{$tag}->{namePlural} ) {
 						Slim::Utils::Strings::storeExtraStrings(
-							[{
-							strings => { EN => $customTags->{$tag}->{name}},
-							token   => $tag,
-							},
-							{
-							strings => { EN => $customTags->{$tag}->{namePlural}},
-							token   => $tag . "_PLURAL",
-							}
-							]);
+							[
+								{ strings => { EN => $customTags->{$tag}->{name}}, token   => $tag },
+								{ strings => { EN => $customTags->{$tag}->{namePlural}}, token   => $tag . "_PLURAL" }
+							]
+						);
 						$changed = 1;
 					}
 				}
