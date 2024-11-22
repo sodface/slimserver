c4d4310dab10a0f8ff65528b8a13749444727cce
Michael Herger <michael@herger.net>
2024-11-22 15:01:39 +0100

    Redirect to the wizard if firing up the settings before an initial setup.
    pCP allows a user to directly access the settings, rather than the landing page. In this case we should still show the wizard on a new setup. See https://forums.slimdevices.com/node/1735686.

--- Slim/Web/Settings/Server/Index.pm
+++ Slim/Web/Settings/Server/Index.pm
@@ -7,14 +7,26 @@ package Slim::Web::Settings::Server::Index;
 # version 2.
 
 use strict;
+use HTTP::Status qw(RC_MOVED_TEMPORARILY);
+
 use base qw(Slim::Web::Settings);
+use Slim::Utils::Prefs;
+
+my $prefs = preferences('server');
 
 sub page {
 	return Slim::Web::HTTP::CSRF->protectURI('settings/index.html');
 }
 
 sub handler {
-	my ($class, $client, $paramRef) = @_;
+	my ($class, $client, $paramRef, $pageSetup, $httpClient, $response) = @_;
+
+	# redirect to the setup wizard if it has never been run before
+	if (!$prefs->get('wizardDone')) {
+		$response->code(RC_MOVED_TEMPORARILY);
+		$response->header('Location' => '/settings/server/wizard.html');
+		return Slim::Web::HTTP::filltemplatefile($class->page, $paramRef);
+	}
 
 	return $class->SUPER::handler($client, $paramRef);
 }
